af92c77e86d711aa83356779b88dbc2b5ed526ee
Rodrick <other@deadinsideartist.art>
2024-08-20 18:51:19 -0500

    Fixing character encoding issue

--- Update.py
+++ Update.py
@@ -10,16 +10,16 @@ q_image = input("\nWould you like to add an image (y or n)? ")
 if q_image == "y":
     image = input("Image file: ")
     c_image = input("Image Caption:\n    ")
-    
-with open('site/_data/entries.json', 'r+') as f:
+
+with open('site/_data/entries.json', 'r+', encoding='utf-8') as f:
         last_color = json.load(f)
-        last_color = last_color["last_color"]        
+        last_color = last_color["last_color"]
 color = input("\nWhat color do you want? Last color: " + last_color + "\npurple, green, green-alt, blood, blood-alt, pink, pink-alt, black, white\n    ")
 
 ## -- Updating data -- ##
 
 try:
-    with open('site/_data/entries.json', 'r+') as f:
+    with open('site/_data/entries.json', 'r+', encoding='utf-8') as f:
         data = json.load(f)
         data["num"] += 1
         data["last_color"] = color
@@ -35,13 +35,13 @@ try:
             new_entry["image"] = [c_image, image]
         else:
             new_entry["image"] = None
-            
+
         data["list"].append(new_entry)
 
         f.seek(0)
-        json.dump(data, f, indent=4)
+        json.dump(data, f, indent=4, ensure_ascii=False)
         f.truncate()
-    
+
     print("\nEntry " + str(data["num"]) + " written. <3")
 except Exception as e:
     print("\nAn error has occurred:")
